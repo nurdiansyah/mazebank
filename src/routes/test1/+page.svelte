955c561b0da619b96ddb7e417c12bae3f78560b8
hanifhahn <hanifhahn23@gmail.com>
2023-07-03 17:36:32 +0700

    layout vertical 1

---
+++ src/routes/test1/+page.svelte
@@ -0,0 +1,114 @@
+<div class="w-[1920px] h-[1080px] relative bg-white">
+	<div class="w-[1920px] h-[195px] left-0 top-0 absolute">
+		<div class="w-[1920px] h-[195px] left-0 top-0 absolute bg-white" />
+		<img
+			class="w-[110px] h-[110px] left-[48px] top-[42px] absolute"
+			src="Logo_Maze-Bank.png"
+			alt=""
+		/>
+		<div class="w-[600px] h-[110px] left-[184px] top-[42px] absolute">
+			<div class="w-[600px] h-[72px] left-0 top-0 absolute text-black text-[48px] font-semibold">
+				Maze Bank
+			</div>
+			<div class="w-[600px] h-12 left-0 top-[62px] absolute text-black text-[32px] font-normal">
+				Cabang East Los Santos
+			</div>
+		</div>
+		<div class="w-[400px] h-[130px] left-[1441px] top-[36px] absolute">
+			<div
+				class="w-[400px] h-[87px] left-0 top-0 absolute text-right text-black text-[58px] font-semibold"
+			>
+				23 : 59 : 00
+			</div>
+			<div
+				class="w-[400px] h-[54px] left-0 top-[76px] absolute text-right text-black text-[36px] font-normal"
+			>
+				23 Juli 2023
+			</div>
+		</div>
+	</div>
+
+	<div class="w-[1920px] h-[50px] left-0 top-[195px] absolute bg-mainColor2">
+		<div class="w-[1920px] h-[50px] left-0 top-0 absolute bg-orange-500" />
+		<div
+			class="left-[19px] top-[11px] absolute text-white text-[24px] font-medium tracking-wider animate-marquee whitespace-nowrap text-mainColor1"
+		>
+			<span
+				>Sarapan Gratis! Cukup bayar dengan Maze Point. Download Maze Bank App dan upgrade ke Maze
+				Prime dapatkan 25 ribu point</span
+			>
+
+			<span> - </span>
+
+			<span
+				>Belanja apa saja di Mazemart dan dapatkan tambahan diskon 10% setiap hari menggunakan jenis
+				kartu kredit Maze Bank. Garansi Termurah!</span
+			>
+
+			<span> - </span>
+
+			<span
+				>Santap Nikmat Tetap Hemat Bikin jadi Hidup! Nikmati diskon hingga 50% dengan pembayaran
+				menggunakan kartu kredit Maze Bank.</span
+			>
+		</div>
+	</div>
+
+	<div class="w-[1920px] h-[835px] left-0 top-[245px] absolute">
+		<div class="w-[1920px] h-[835px] left-0 top-0 absolute bg-sky-700" />
+		<div class="w-[630px] h-[770px] left-[1267px] top-[33px] absolute">
+			<div class="w-[630px] h-[210px] left-0 top-[28px] absolute">
+				<div class="w-[630px] h-[210px] left-0 top-0 absolute bg-orange-500 rounded-lg shadow" />
+				<div class="left-[40px] top-[19px] absolute text-white text-[72px] font-bold">Loket 1</div>
+				<div
+					class="left-[40px] top-[112px] absolute text-white text-[48px] font-normal tracking-widest"
+				>
+					PASIEN UMUM
+				</div>
+				<div
+					class="w-60 h-[108px] left-[350px] top-[19px] absolute text-right text-yellow-400 text-[72px] font-bold"
+				>
+					A509
+				</div>
+			</div>
+			<div class="w-[630px] h-[210px] left-0 top-[280px] absolute">
+				<div class="w-[630px] h-[210px] left-0 top-0 absolute bg-orange-500 rounded-lg shadow" />
+				<div class="left-[40px] top-[19px] absolute text-white text-[72px] font-bold">Loket 2</div>
+				<div
+					class="left-[40px] top-[112px] absolute text-white text-[48px] font-normal tracking-widest"
+				>
+					PASIEN BPJS
+				</div>
+				<div
+					class="w-60 h-[108px] left-[350px] top-[19px] absolute text-right text-yellow-400 text-[72px] font-bold"
+				>
+					B109
+				</div>
+			</div>
+			<div class="w-[630px] h-[210px] left-0 top-[532px] absolute">
+				<div class="w-[630px] h-[210px] left-0 top-0 absolute bg-orange-500 rounded-lg shadow" />
+				<div class="left-[40px] top-[19px] absolute text-white text-[72px] font-bold">Loket 3</div>
+				<div
+					class="left-[40px] top-[112px] absolute text-white text-[48px] font-normal tracking-widest"
+				>
+					PASIEN UMUM
+				</div>
+				<div
+					class="w-60 h-[108px] left-[350px] top-[19px] absolute text-right text-yellow-400 text-[72px] font-bold"
+				>
+					A508
+				</div>
+			</div>
+		</div>
+
+		<div class="w-[1195px] h-[770px] left-[36px] top-[33px] absolute">
+			<div class="w-[1195px] h-[770px] left-0 top-0 absolute rounded-lg shadow">
+				<img
+					class="w-[1195px] h-[796px] left-0 top-[-13px] absolute"
+					src="https://via.placeholder.com/1195x796"
+					alt=""
+				/>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/test2/+page.svelte
@@ -0,0 +1,14 @@
+<div class="flex flex-col h-screen">
+    <div class="bg-mainColor1 h-25">
+      <!-- Konten Baris Pertama -->
+      <p>Baris Konten Pertama</p>
+    </div>
+    <div class="bg-mainColor2 h-25">
+      <!-- Konten Baris Kedua -->
+      <p>Baris Konten Kedua</p>
+    </div>
+    <div class="bg-mainColor3 h-50">
+      <!-- Konten Baris Ketiga -->
+      <p>Baris Konten Ketiga</p>
+    </div>
+  </div>
