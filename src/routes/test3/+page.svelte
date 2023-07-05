34e33cfe296be6881156f882758aa20321ab18a4
hanifhahn <hanifhahn23@gmail.com>
2023-07-05 13:50:27 +0700

    Layout Vertical 3

--- src/routes/test3/+page.svelte
+++ src/routes/test3/+page.svelte
@@ -1,10 +1,64 @@
-<div class="grid grid-row-2">
-	<div class=" bg-mainColor2">
-		<!-- Content for the first row (2/3 width) -->
-		<h1>biduawbnoa</h1>
+<script>
+	import { time, date } from '../../addons/DateAndTime.svelte';
+</script>
+
+<!-- Header Content -->
+<!-- Informasi Bank -->
+<header class="bg-mainColor1 py-5 ps-8 pe-8">
+	<div class="flex flex-col items-center md:flex-row">
+		<div class="w-full md:w-1/2 flex items-center justify-start">
+			<img class="w-full md:w-auto max-w-[8rem]" src="Logo_Maze-Bank.png" alt="" />
+			<div class="item-body px-4 my-auto">
+				<span
+					class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
+					>Maze Bank</span
+				>
+				<br />
+				<span class="text-2xl md:text-2xl text-right text-grey-dark">Cabang East Los Santos</span>
+			</div>
+		</div>
+
+		<div class="w-full md:w-1/2 flex items-center justify-end">
+			<div class="item-body px-4 my-auto text-right">
+				<span
+					class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
+					>{$time}</span
+				>
+				<br />
+				<span class="text-2xl md:text-2xl text-grey-dark">{$date}</span>
+			</div>
+		</div>
 	</div>
-	<div class=" bg-mainColor4">
-		<!-- Content for the second row (1/3 width) -->
-		<h1>bd3wuiqh</h1>
+</header>
+
+<!-- Side Content -->
+<!-- Running Text Berita -->
+<div
+	class="bg-mainColor2 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0 relative overflow-x-hidden"
+>
+	<div class="py-2 animate-marquee whitespace-nowrap text-mainColor1 font-inter md:font-base">
+		<span class="text-2xl mx-2"
+			>Sarapan Gratis! Cukup bayar dengan Maze Point. Download Maze Bank App dan upgrade ke Maze
+			Prime dapatkan 25 ribu point</span
+		>
+
+		<span class="text-2xl mx-2"> - </span>
+
+		<span class="text-2xl mx-2"
+			>Belanja apa saja di Mazemart dan dapatkan tambahan diskon 10% setiap hari menggunakan jenis
+			kartu kredit Maze Bank. Garansi Termurah!</span
+		>
+
+		<span class="text-2xl mx-2"> - </span>
+
+		<span class="text-2xl mx-2"
+			>Santap Nikmat Tetap Hemat Bikin jadi Hidup! Nikmati diskon hingga 50% dengan pembayaran
+			menggunakan kartu kredit Maze Bank.</span
+		>
+
+		<span class="text-2xl mx-2"> - </span>
 	</div>
 </div>
+
+<!-- Body Content -->
+
--- src/routes/vertical3/+page.svelte
+++ src/routes/vertical3/+page.svelte
@@ -2,76 +2,75 @@
 	import { time, date } from '../../addons/DateAndTime.svelte';
 </script>
 
-<!-- Header Content -->
-<!-- Informasi Bank -->
-<header class="bg-mainColor1 py-5 ps-8 pe-8">
-	<div class="flex flex-col items-center md:flex-row">
-		<div class="w-full md:w-1/2 flex items-center justify-start">
-			<img class="w-full md:w-auto max-w-[8rem]" src="Logo_Maze-Bank.png" alt="" />
-			<div class="item-body px-4 my-auto">
-				<span
-					class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
-					>Maze Bank</span
-				>
-				<br />
-				<span class="text-2xl md:text-2xl text-right text-grey-dark">Cabang East Los Santos</span>
+<div class="flex flex-col h-screen max-h-screen overflow-x-hidden">
+	<!-- Header Content -->
+	<!-- Informasi Lembaga -->
+	<header class="bg-mainColor1 mx-11 py-7">
+		<div class="flex flex-col items-center md:flex-row">
+			<div class="w-full md:w-1/2 flex items-center justify-start">
+				<img src="/asset/asset_header/Logo_Maze-Bank.png" alt="Logo" class="h-28" />
+				<div class="item-body px-4 my-auto">
+					<span
+						class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
+						>Maze Bank</span
+					>
+					<br />
+					<span class="text-2xl md:text-2xl text-right text-grey-dark">Cabang East Los Santos</span>
+				</div>
+			</div>
+
+			<div class="w-full md:w-1/2 flex items-center justify-end">
+				<div class="item-body px-4 my-auto text-right">
+					<span
+						class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
+						>{$time}</span
+					>
+					<br />
+					<span class="text-2xl md:text-2xl text-grey-dark">{$date}</span>
+				</div>
 			</div>
 		</div>
+	</header>
 
-		<div class="w-full md:w-1/2 flex items-center justify-end">
-			<div class="item-body px-4 my-auto text-right">
-				<span
-					class="text-3xl md:text-5xl no-underline text-grey-darkest hover:text-blue-dark font-poppins md:font-semibold"
-					>{$time}</span
-				>
-				<br />
-				<span class="text-2xl md:text-2xl text-grey-dark">{$date}</span>
-			</div>
+	<!-- Side Content -->
+	<!-- Running Text -->
+	<div class="bg-mainColor2 h-25 px-3">
+		<div class="py-2 animate-marquee whitespace-nowrap text-mainColor1 font-inter md:font-base">
+			<span class="text-2xl mx-2"
+				>Sarapan Gratis! Cukup bayar dengan Maze Point. Download Maze Bank App dan upgrade ke Maze
+				Prime dapatkan 25 ribu point</span
+			>
+
+			<span class="text-2xl mx-2"> - </span>
+
+			<span class="text-2xl mx-2"
+				>Belanja apa saja di Mazemart dan dapatkan tambahan diskon 10% setiap hari menggunakan jenis
+				kartu kredit Maze Bank. Garansi Termurah!</span
+			>
+
+			<span class="text-2xl mx-2"> - </span>
+
+			<span class="text-2xl mx-2"
+				>Santap Nikmat Tetap Hemat Bikin jadi Hidup! Nikmati diskon hingga 50% dengan pembayaran
+				menggunakan kartu kredit Maze Bank.</span
+			>
+
+			<span class="text-2xl mx-2"> - </span>
 		</div>
 	</div>
-</header>
 
-<!-- Side Content -->
-<!-- Running Text Berita -->
-<div
-	class="bg-mainColor2 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0 relative overflow-x-hidden"
->
-	<div class="py-2 animate-marquee whitespace-nowrap text-mainColor1 font-inter md:font-base">
-		<span class="text-2xl mx-2"
-			>Sarapan Gratis! Cukup bayar dengan Maze Point. Download Maze Bank App dan upgrade ke Maze
-			Prime dapatkan 25 ribu point</span
-		>
+	<!-- Main Content -->
+	<div class="bg-mainColor3 h-full flex flex-col p-7">
+		<!-- Test Gambar -->
+		<!-- <img class="w-full h-full rounded-lg shadow-right-bottom" src="/asset/asset_main_content/picture-test-3.png" alt="" /> -->
 
-		<span class="text-2xl mx-2"> - </span>
-
-		<span class="text-2xl mx-2"
-			>Belanja apa saja di Mazemart dan dapatkan tambahan diskon 10% setiap hari menggunakan jenis
-			kartu kredit Maze Bank. Garansi Termurah!</span
-		>
-
-		<span class="text-2xl mx-2"> - </span>
-
-		<span class="text-2xl mx-2"
-			>Santap Nikmat Tetap Hemat Bikin jadi Hidup! Nikmati diskon hingga 50% dengan pembayaran
-			menggunakan kartu kredit Maze Bank.</span
-		>
-
-		<span class="text-2xl mx-2"> - </span>
-	</div>
-</div>
-
-<!-- Body Content -->
-<!-- <div class="bg-mainColor3 w-full flex h-screen overflow-hidden"> -->
-<div class="bg-mainColor3 w-full h-auto">
-	<div class="flex flex-col h-screen">
-		<div class="flex-1 p-4">
-			<iframe
-				class="w-full h-full rounded-lg shadow-right-bottom"
-				src="https://www.youtube.com/embed/KMPnVroI2vE?autoplay=0&loop=1"
-				title="YouTube video player"
-				frameborder="0"
-				allowfullscreen
-			/>
-		</div>
+		<!-- Test Video Youtube -->
+		<iframe
+			class="w-full h-full rounded-lg shadow-right-bottom"
+			src="https://www.youtube.com/embed/KMPnVroI2vE?autoplay=0&loop=1"
+			title="YouTube video player"
+			frameborder="0"
+			allowfullscreen
+		/>
 	</div>
 </div>
