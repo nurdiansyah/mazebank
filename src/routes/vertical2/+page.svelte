9deb9ee0bce3e937614d4eb8c24266f70a14a0e3
hanifhahn <hanifhahn23@gmail.com>
2023-07-10 12:57:21 +0700

    Vertical 2 done

---
+++ src/routes/vertical2/+page.svelte
@@ -0,0 +1,13 @@
+<div class="bg-black w-full h-screen flex-grow">
+	<!-- Test Gambar -->
+	<img class="w-full h-screen" src="/asset/asset_main_content/vertical-poster-test-1.svg" alt="" />
+
+	<!-- Test Video Youtube -->
+	<!-- <iframe
+		class="w-full h-full"
+		src="https://www.youtube.com/embed/KMPnVroI2vE?autoplay=0&loop=1"
+		title="YouTube video player"
+		frameborder="0"
+		allowfullscreen
+	/> -->
+</div>
